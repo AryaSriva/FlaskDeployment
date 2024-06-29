5a8cbdbce30191314d7e799fadb8e996a9c5a2de
AryaSriva <87484641+AryaSriva@users.noreply.github.com>
2024-06-28 22:55:34 -0400

    added deployment steps css file and jupyter notebook file

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 from flask import Flask, request, render_template
-import pickle
-import pandas as pd
 import numpy as np
-from sklearn import linear_model
+import pickle
 
 app = Flask(__name__)
-
-# build model for predicting price charged by Pink Cab in Boston
-df = pd.read_csv("finalCabData.csv")
-filteredDf = df[(df["City"] == "BOSTON MA")]
-filteredDf = filteredDf[filteredDf["Company"] == "Pink Cab"]
-x = np.array(filteredDf[["KM Travelled", "Cost of Trip"]])
-y = np.array(filteredDf["Price Charged"])
-model = linear_model.LinearRegression()
-model.fit(x, y)
+model = pickle.load(open("model.pkl", "rb"))
 
 
-@app.route("/")
+@app.route("/")  # home root
 def home():
     return render_template("index.html")
 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST"])  # prediction post method for submission
 def predict():
     # predict price charged based on user inputted distance travelled and cost of trip
     args = [int(x) for x in request.form.values()]
